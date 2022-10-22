00aeb0384fb8c6ac22f3f4b9bbf7a1dedbcf30ee
ctola91 <ctolapacheco@gmail.com>
2022-10-22 07:33:28 -0400

    Use Model class instead of declare

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,19 +1,26 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes, Model } from 'sequelize';
 import db from '../db/connection';
 
-const User = db.define('users', {
-    username: {
-        type: DataTypes.STRING
-    },
-    userpass: {
-        type: DataTypes.STRING
-    },
-    userrole: {
-        type: DataTypes.STRING
-    },
-    isActive: {
-        type: DataTypes.BOOLEAN
-    },
-}, { underscored: true, createdAt: 'created_at', updatedAt: 'updated_at' });
+class User extends Model {
+    declare username: string
+    declare userpass: string
+    declare userrole: string
+    declare isActive: boolean
+}
+
+User.init({
+        username: {
+            type: DataTypes.STRING
+        },
+        userpass: {
+            type: DataTypes.STRING
+        },
+        userrole: {
+            type: DataTypes.STRING
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN
+        },
+},  { sequelize: db, tableName: 'users', underscored: true, createdAt: 'created_at', updatedAt: 'updated_at' })
 
 export default User;
