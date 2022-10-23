ce66ec7f3c9d7f66e13edb14d3a22a364043c4a0
ctola91 <ctolapacheco@gmail.com>
2022-10-22 23:28:54 -0400

    Fix login controller

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,17 +1,26 @@
 import { Request, Response } from 'express';
 import { json } from "sequelize/types";
 import { getUserByUsername } from '../services/UserService';
-import { isValidPassword, getToken } from '../services/AuthService';
-import User from '../types/User';
+import { getToken } from '../services/AuthService';
 
 export const login = async (req: Request, res: Response) => {
-    let body = req.body;
-    const user = await getUserByUsername(body.email);
-    if (!user) {
-        return res.status(401).json({
-            message: "User or password incorrect"
-        });
+    try {
+        let body = req.body;
+        const user = await getUserByUsername(body.email);
+        if (!user) {
+            return res.status(401).json({
+                message: "User or password incorrect"
+            });
+        }
+        // TODO: VALIDATE PASSWORD
+        const token = getToken(user);
+        res.json({ user, token })
+    } catch (e) {
+        console.log(e);
+        if (e instanceof Error) {
+            res.status(400).json({ message: e.message });
+        } else {
+            console.log(e);
+        }
     }
-    // TODO: VALIDATE PASSWORD
-    const token = getToken(user);
 }
