912a11ee54245452370e07ada6a94123fcd347b9
ctola91 <ctolapacheco@gmail.com>
2022-10-25 22:56:19 -0400

    add repository methods

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -4,6 +4,10 @@ const findOneByEmail = async (email: string) => {
     return await User.findOne({ where: { email } });
 }
 
+const findOneById = async (id: number) => {
+    return await User.findOne({ where: { id } })
+}
+
 const findAndCountAll = async (from = 0, limit = 5, filters: any, attributes: any) => {
     const data = await User.findAndCountAll({
         limit, offset: from,
@@ -25,8 +29,36 @@ const createUser = async (firstName: string, lastName: string, email: string, pa
     return user;
 }
 
+const modifyUser = async (userId: number, firstName: string, lastName: string, email: string, password: string, role: string) => {
+    const currentUser = await User.findOne({ where: { id: userId } });
+    if (currentUser) {
+        currentUser.firstName = firstName;
+        currentUser.lastName = lastName;
+        currentUser.email = email;
+        currentUser.password = password;
+        currentUser.role = role;
+        return await currentUser.save();
+    }
+    return null;
+}
+
+const deleteUser = async (id: number) => {
+    const deleteActive = {
+        isActive: false,
+    }
+    const currentUser = await User.findOne({ where: { id } });
+    if (currentUser) {
+        currentUser.isActive = deleteActive.isActive;
+        return await currentUser.save();
+    }
+    return null;
+}
+
 export default {
     findOneByEmail,
+    findOneById,
     findAndCountAll,
-    createUser
+    createUser,
+    modifyUser,
+    deleteUser
 }
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -11,6 +11,10 @@ export const findAndCountAll = async (from = 0, limit = 5, attributes: any) => {
     return await UserRepository.findAndCountAll(from, limit, defaultFilters, attributes);
 }
 
-export const createUser = async (firstName: string, lastName: string, email: string, password: string, userrole: string) => {
-    return await UserRepository.createUser(firstName, lastName, email, bcrypt.hashSync(password, 10), userrole);
+export const createUser = async (firstName: string, lastName: string, email: string, password: string, role: string) => {
+    return await UserRepository.createUser(firstName, lastName, email, bcrypt.hashSync(password, 10), role);
+}
+
+export const updateUser = async (userId: number, firstName: string, lastName: string, email: string, password: string, role: string) => {
+    return await UserRepository.modifyUser(userId, firstName, lastName, email, password, role);
 }
